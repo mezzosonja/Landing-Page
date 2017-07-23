ec7c4f5e2fd350258d6968db16cc49f1956d272d
MEZZOSONJA\Sonja <mezzosonja@gmail.com>
2017-07-23 20:37:05 +0200

    first

---
+++ js/home.js
@@ -0,0 +1,50 @@
+//Navbar
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+
+})(jQuery); 
+
+// Carousel 
+
+$(document).ready(function() {
+
+    $('.carousel').carousel({
+        interval: 6000,
+        pause: "false"
+    });
+
+    /* affix the navbar after scroll below header */
+    $('#nav').affix({
+        offset: {
+            top: $('header').height() - $('#nav').height()
+        }
+    });
+});
